3b118e727424a0c118b39d54ffbee75a17e1943d
gcorre02 <guilherme.ctr@gmail.com>
2014-04-08 13:18:09 +0100

    created a stub for the test class for quiz. TDD from here on.

--- QuizSystem/src/quizData/Quiz.java
+++ QuizSystem/src/quizData/Quiz.java
@@ -20,5 +20,5 @@ public class Quiz {
 		quizQuestions.add(newQuestion);
 	}
 	
-	
+
 }
---
+++ QuizSystem/src/test/QuizTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class QuizTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testQuiz() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddQuestion() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
