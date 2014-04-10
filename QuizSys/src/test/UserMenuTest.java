cda4e6b410c0c701493047f20b54c9eef4b1c563
gcorre02 <guilherme.ctr@gmaill.com>
2014-04-10 21:48:53 +0100

    created a stub for the user menu test.

---
+++ QuizSys/src/test/UserMenuTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import menu.UserMenu;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import persistence.Loader;
+import persistence.Saver;
+import userInterface.UserInterface;
+
+public class UserMenuTest {
+	UserMenu um;
+	UserInterface ui;
+	Loader l;
+	Saver s;
+	String source;
+	
+	@Before
+	public void setUp() throws Exception {
+		source = "testFiles";
+		ui = mock(UserInterface.class);
+		l = new Loader(source);
+		s = new Saver(source);
+		um = new UserMenu(l,s,ui);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		source = null;
+		ui = null;
+		l = null;
+		s = null;
+		um = null;
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
