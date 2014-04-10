e9e111c08ff051c07e7de3368b58ccc85974b1fa
gcorre02 <guilherme.ctr@gmaill.com>
2014-04-11 00:44:15 +0100

    wrote impl of the deleteuser() under userMenu.

--- QuizSys/src/menu/UserMenu.java
+++ QuizSys/src/menu/UserMenu.java
@@ -1,5 +1,6 @@
 package menu;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import lombok.AllArgsConstructor;
@@ -11,6 +12,9 @@ import userInterface.UserInterface;
 /**
  * Handles all interactions between the user and the user data.
  * Also calls the next menu.
+ * 
+ * TODO<implement> change username to a different name, without compromising it's dependencies!!!
+ * 
  * @author Guilherme
  *
  */
@@ -31,6 +35,9 @@ public class UserMenu {
 	}
 	public void runMenu(String menu){
 		char choice = ui.getUserAnswer(menu);
+		//debug
+		System.out.println(choice);
+		//\debug
 		switch(choice){
 		case 'A':
 			createNewUser();
@@ -53,21 +60,33 @@ public class UserMenu {
 			break;
 		}
 	}
+	
 	private void closeProgram() {
 		System.out.println("Shutting down, thank you.");
-		// TODO Auto-generated method stub
-		
 	}
+	
 	private void login() {
 		System.out.println("Please enter your username:");
 		// TODO Auto-generated method stub
 		
 	}
+	
 	private void deleteUser() {
 		System.out.println("Please enter the username you wish to delete:");
 		// TODO Auto-generated method stub
+		String user = ui.readFromUser();
+		try {
+			if(s.deleteUser(user)){
+				System.out.println("user was deleted successfully");
+			}else{
+				System.out.println("User does not exist.");
+			}
+		} catch (IOException e) {
+			System.out.println("Couldn't delete user, please try again");
+		}
 		
 	}
+	
 	private void createNewUser() {
 		System.out.println("Please enter the new username:");
 		// TODO Auto-generated method stub
