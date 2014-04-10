68c5b388548cdfd0b4f2022c031a7fcbfd4def92
gcorre02 <guilherme.ctr@gmaill.com>
2014-04-10 21:18:43 +0100

    wrote the readfromuser() under UserInterface.

--- QuizSys/src/test/UserInterfaceTest.java
+++ QuizSys/src/test/UserInterfaceTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.After;
 import org.junit.Before;
--- QuizSys/src/userInterface/UserInterface.java
+++ QuizSys/src/userInterface/UserInterface.java
@@ -2,4 +2,8 @@ package userInterface;
 
 public class UserInterface {
 
+	public String readFromUser() {
+		return System.console().readLine();
+	}
+
 }
