d9c86d574f5dd4b1fc14d6e2e1aecc961721ea6a
gcorre02 <guilherme.ctr@gmail.com>
2014-04-15 18:48:11 +0100

    created a LaunchPlayer with a main to launch player Menus. Created a basicMain class to run both

--- QuizSys/src/main/BasicMain.java
+++ QuizSys/src/main/BasicMain.java
@@ -11,16 +11,8 @@ import menu.LoginMenu;
 public class BasicMain {
 	
 	public static void main(String[] args) {
-		final String FOLDER = "ExecTry";
-		Saver s = new Saver(FOLDER);
-		Loader l = new Loader(FOLDER);
-		UserInterface ui = new UserInterface();
-		LoginMenu lm = new LoginMenu(l,s,ui);
-		lm.run();
-        PlayerSaver ps = new PlayerSaver(FOLDER);
-        PlayerLoader pl = new PlayerLoader(FOLDER);
-        PlayerLoginMenu plm = new PlayerLoginMenu(pl,ps,ui);
-        plm.run();
+        final String[] FOLDER = {"ExecTry"};
+		LaunchPlayer.main(FOLDER);
 	}
 
 }
---
+++ QuizSys/src/main/LaunchPlayer.java
@@ -0,0 +1,19 @@
+package main;
+
+import persistence.PlayerLoader;
+import persistence.PlayerSaver;
+import player.PlayerLoginMenu;
+import userInterface.UserInterface;
+
+/**
+ * Created by Admin on 15/04/2014.
+ */
+public class LaunchPlayer {
+    public static void main(String[] args) {
+        PlayerSaver ps = new PlayerSaver(args[0]);
+        PlayerLoader pl = new PlayerLoader(args[0]);
+        UserInterface ui = new UserInterface();
+        PlayerLoginMenu plm = new PlayerLoginMenu(pl,ps,ui);
+        plm.run();
+    }
+}
