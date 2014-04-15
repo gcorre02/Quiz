76fa8d48dffc8c674eddc3c06f81f21603f0a857
gcorre02 <guilherme.ctr@gmail.com>
2014-04-15 17:08:44 +0100

    implemented the Close() under PlayAQuizMenu to update the player info.

--- QuizSys/src/player/PlayAQuizMenu.java
+++ QuizSys/src/player/PlayAQuizMenu.java
@@ -3,12 +3,15 @@ package player;
 import lombok.AllArgsConstructor;
 import persistence.PlayerLoader;
 import persistence.PlayerSaver;
+import quizData.Player;
 import quizData.Question;
 import tools.CollectionPrinter;
 import userInterface.UserInterface;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Admin on 15/04/2014.
@@ -37,7 +40,7 @@ public class PlayAQuizMenu {
 
         System.out.println("Your score is " + score + "%");
 
-        close();
+        close(score);
     }
 
     private double playQuiz() throws IOException {
@@ -72,6 +75,37 @@ public class PlayAQuizMenu {
     /**
      * method used to persist data: score, increase played counter ...
      */
-    private void close() {
+    private void close(Double score) {
+        Player p = pl.getPlayer(playerName);
+        Map<String, String[]> playedQuizzes;
+        Map<String, Double> quizScores;
+
+        if(p.firstPlay()){
+            playedQuizzes = new HashMap<>();
+            String[] currentQuiz = {quiz};
+            playedQuizzes.put(quizOwner, currentQuiz);
+            quizScores = new HashMap<>();
+            quizScores.put((quizOwner+"."+quiz),score);
+
+        }else{
+            playedQuizzes = p.getPlayedQuizzes();
+            quizScores = p.getQuizScores();
+            if(!playedQuizzes.containsKey(quizOwner)
+                    ||!CollectionPrinter.arrayContains(quiz,playedQuizzes.get(quizOwner))) {
+                String[] newQuizzes = {quiz};
+                playedQuizzes.put(quizOwner, newQuizzes);
+            }
+            String quizScoreKey = quizOwner+"."+quiz;
+            Double highScore = quizScores.get(quizScoreKey);
+            if(score > highScore){
+                quizScores.replace(quizScoreKey,score);
+                System.out.println("New High Score for you!");
+            }
+
+        }
+        p.setPlayedQuizzes(playedQuizzes);
+        p.setQuizScores(quizScores);
+
+        ps.savePlayer(p);
     }
 }
--- QuizSys/src/quizData/Player.java
+++ QuizSys/src/quizData/Player.java
@@ -13,10 +13,19 @@ import java.util.Map;
 public class Player {
     private String name;
     private Map<String, String[]> playedQuizzes;
+    private Map<String, Double> quizScores;
 
     public Player(String name){
         this.name = name;
         playedQuizzes = new HashMap<>();
         playedQuizzes.put("No Quizzes Played Yet",new String[0]);
+        quizScores = new HashMap<>();
+        quizScores.put("No Quizzes Played Yet", 0.0);
+    }
+    public boolean firstPlay(){
+        if(playedQuizzes.containsKey("No Quizzes Played Yet"))
+            return true;
+        else
+            return false;
     }
 }
