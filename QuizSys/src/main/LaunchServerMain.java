b76d09ca2f133f1e6faaf1ec174d9f2e8aff4f25
Guilherm Ribeiro <guilherme.ctr@gmail.com>
2014-04-18 19:46:21 +0100

    implemented ServerMain, the class that launches the server.

---
+++ QuizSys/src/main/LaunchServerMain.java
@@ -0,0 +1,13 @@
+package main;
+
+import rmi.LoaderServerLauncher;
+
+/**
+ * Created by user on 18-04-2014.
+ */
+public class LaunchServerMain {
+    public static void main(String[] args) {
+        LoaderServerLauncher lsl = new LoaderServerLauncher();
+        lsl.main(new String[0]);
+    }
+}
--- QuizSys/src/main/ServerMain.java
+++ QuizSys/src/main/ServerMain.java
@@ -8,15 +8,16 @@ public class ServerMain {
 //TODO need to create a server Runner main and a process to get the folder from the server to set it here:
 //STUB
     public static void main(String[] args) {
-        final String[] FOLDER = {"ExecTry"};
+        final String[] FOLDER = {"ExecTry"};//TODO make this an input from the server
 		UserInterface ui = new UserInterface();
+
         String[] strs = {"Go to Player Menus","Go to User Menus"};
         String choices = CollectionPrinter.collectionPrinter('S',CollectionPrinter.toArrayList(strs));
         char choice = ui.getUserAnswer(choices);
         if(choice == 'A')
-            LaunchPlayerLocalVersion.main(FOLDER);
+            LaunchPlayerServerVersion.main(FOLDER);
         else if(choice == 'B')
-            LaunchUserLocalVersion.main(FOLDER);
+            LaunchUserServerVersion.main(FOLDER);
         else
             System.out.println("Couldn't understand input. Bye");
     }
