27a0617a1430605de7fd45307d6603f839dc8978
gcorre02 <guilherme.ctr@gmail.com>
2014-04-19 23:24:14 +0100

    optimized code in server main.

--- QuizSys/src/main/launchers/ServerMain.java
+++ QuizSys/src/main/launchers/ServerMain.java
@@ -8,18 +8,25 @@ import tools.UserInterface;
 //Client
 public class ServerMain {
 //TODO need to create a server Runner main and a process to get the folder from the server to set it here:
-//STUB
     public static void main(String[] args) {
-        final String[] FOLDER = args;//{"testFiles"};//TODO make this an input from the server
-		UserInterface ui = new UserInterface();
+        UserInterface ui = new UserInterface();
 
+        launch(args[0], ui);
+    }
+
+    /**
+     * Separated from main to allow testing.
+     * @param source the folder where the Quiz system file structure is.
+     * @param ui UserInterface for mocking inputs.
+     */
+    public static void launch(String source, UserInterface ui) {
         String[] strs = {"Go to Player Menus","Go to User Menus"};
         String choices = CollectionTools.collectionPrinter('S', CollectionTools.toArrayList(strs));
         char choice = ui.getUserAnswer(choices);
         if(choice == 'A')
-            LaunchPlayerServerVersion.main(FOLDER);
+            LaunchPlayerServerVersion.main(new String[]{source});
         else if(choice == 'B')
-            LaunchUserServerVersion.main(FOLDER);
+            LaunchUserServerVersion.main(new String[]{source});
         else
             System.out.println("Couldn't understand input. Bye");
     }
