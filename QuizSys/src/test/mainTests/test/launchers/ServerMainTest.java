f9985dbc71aa7851685f8ccd54f5f34303e638cc
gcorre02 <guilherme.ctr@gmail.com>
2014-04-19 23:53:21 +0100

    tested ServerMain.launch()

---
+++ QuizSys/src/test/mainTests/test/launchers/ServerMainTest.java
@@ -0,0 +1,37 @@
+package test.mainTests.test.launchers;
+
+import main.launchers.ServerMain;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import tools.UserInterface;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Admin on 19/04/2014.
+ */
+public class ServerMainTest {
+    String source;
+    UserInterface ui;
+
+    @Before
+    public void setUp() throws Exception {
+        source = "testFiles";
+        ui = mock(UserInterface.class);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        source = null;
+    }
+
+    @Test
+    public void testLaunch() throws Exception {
+        when(ui.getUserAnswer(anyString())).thenReturn('D');
+        when(ui.printToUser(anyString())).thenCallRealMethod();
+        ServerMain.launch(source,ui);
+        verify(ui).printToUser("Couldn't understand input. Bye");
+    }
+}
