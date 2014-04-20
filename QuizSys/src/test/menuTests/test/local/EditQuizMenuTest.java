f85f7cba0a01549399fdecc47262c3f0346fa427
gcorre02 <guilherme.ctr@gmail.com>
2014-04-20 13:53:52 +0100

    wrote propper tests for EditQuizMenu test.

--- QuizSys/src/test/menuTests/test/local/EditQuizMenuTest.java
+++ QuizSys/src/test/menuTests/test/local/EditQuizMenuTest.java
@@ -11,8 +11,10 @@ import persistence.SaverInterface;
 import tools.UserInterface;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-import static org.junit.Assert.fail;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class EditQuizMenuTest {
@@ -46,55 +48,45 @@ public class EditQuizMenuTest {
 
 	@Test
 	public final void testRun() {
-		fail("Not yet implemented"); // TODO
+		//mock
+        when(ui.getUserAnswer(anyString())).thenReturn('D');
+        //launch
+        eqm.run();
+        //test
+        verify(ui).printToUser("You will be returned to the previous menu.");
 	}
 
-	@Test
-	public final void testRunMenu() {
-		fail("Not yet implemented"); // TODO
-	}
 
-	@Test
-	public final void testEditQuizMenu() {
-		fail("Not yet implemented"); // TODO
-	}
-	//stub question dellete, needs a propper test
 	@Test
 	public final void testDeleteQuestion() throws IOException{
+		//expected
+        String expected = l.getQuizQuestionsConfig(user,quizName).get(0);
 		//setup
 		when(ui.getUserAnswer(anyString())).thenReturn('B','D');
 		when(ui.readFromUser()).thenReturn("0");
 		//exec
 		eqm.run();
+        //actuals
+        ArrayList<String> actuals = l.getQuizQuestionsConfig(user, quizName);
 		//test
-
-		//Quiz quiz = l.getQuizObject(user, quizName);
-		//System.out.println(CollectionTools.collectionPrinter('0', quiz.getQuizQuestions()));
-		//System.out.println(CollectionTools.collectionPrinter('S',l.getQuizQuestionsConfig(user, quizName)));
-
-		fail("Not yet implemented"); // TODO
-
+        assertFalse(actuals.contains(expected));
 	}
 
-	//stub question dellete, needs a propper test
 	@Test
 	public final void testAddQuestion() throws IOException{
+		//expected
+        String expected = "What type of moron is Moe Sizlack?";
 		//setup
 		when(ui.getUserAnswer(anyString())).thenReturn('A','D');
 		when(ui.readFromUser()).thenReturn("What type of moron is Moe Sizlack?");
 		//exec
 		eqm.run();
+        //actuals
+        ArrayList<String> actuals = l.getQuizQuestionsConfig(user,quizName);
 		//test
-
-		//Quiz quiz = l.getQuizObject(user, quizName);
-		//System.out.println(CollectionTools.collectionPrinter('0', quiz.getQuizQuestions()));
-		//System.out.println(CollectionTools.collectionPrinter('S',l.getQuizQuestionsConfig(user, quizName)));
-
-		fail("Not yet implemented"); // TODO
-
+        assertTrue(actuals.contains(expected));
 	}
 
-	//stub question dellete, needs a propper test
 	@Test
 	public final void testEditQuestion() throws IOException{
 		//setup
@@ -103,12 +95,6 @@ public class EditQuizMenuTest {
 		//exec
 		eqm.run();
 		//test
-
-		//Quiz quiz = l.getQuizObject(user, quizName);
-		//System.out.println(CollectionTools.collectionPrinter('0', quiz.getQuizQuestions()));
-		//System.out.println(CollectionTools.collectionPrinter('S',l.getQuizQuestionsConfig(user, quizName)));
-
-		fail("Not yet implemented"); // TODO
-
-	}
+        verify(ui).printToUser("Please enter the number for the question you wish to edit :");
+    }
 }
