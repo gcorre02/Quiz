5c4fd98c70bb48bdfa6c6b2c6601f745d2921c5b
gcorre02 <guilherme.ctr@gmail.com>
2014-04-20 01:32:32 +0100

    wrote javadocs for LaunchUserLocalVersion class.

--- QuizSys/src/main/LocalUserLaunchers/LaunchUserLocalVersion.java
+++ QuizSys/src/main/LocalUserLaunchers/LaunchUserLocalVersion.java
@@ -8,15 +8,32 @@ import persistence.SaverInterface;
 import tools.UserInterface;
 
 /**
+ * Launches the local User System based on the source file set up.
+ *
  * Created by Admin on 15/04/2014.
  */
 public class LaunchUserLocalVersion {
     private static LoaderInterface l;
     private static SaverInterface s;
+
+    /**
+     * Launches the UserSystem
+     *
+     * @param args the [0] position indicates the path where the file structure lies.
+     */
     public static void main(String[] args) {
-        s = new Saver(args[0]);
-        l = new Loader(args[0]);
         UserInterface ui = new UserInterface();
+        launch(args[0], ui);
+    }
+
+    /**
+     * separated method for inputting a UserInterface mock.
+     * @param source The path to the folder where the file structure is.
+     * @param ui a potential mock of user interface.
+     */
+    private static void launch(String source, UserInterface ui) {
+        s = new Saver(source);
+        l = new Loader(source);
         LoginMenu lm = new LoginMenu(l,s,ui);
         lm.run();
     }
