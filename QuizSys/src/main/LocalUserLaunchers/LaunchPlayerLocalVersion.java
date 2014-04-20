a8e90983abc507bbe51a8092807e120a392179e8
gcorre02 <guilherme.ctr@gmail.com>
2014-04-20 01:04:46 +0100

    refactored LaunchPlayerLocalVersion to launch the system by passing in a UserInterface mock.

--- QuizSys/src/main/LocalUserLaunchers/LaunchPlayerLocalVersion.java
+++ QuizSys/src/main/LocalUserLaunchers/LaunchPlayerLocalVersion.java
@@ -6,13 +6,33 @@ import player.PlayerLoginMenu;
 import tools.UserInterface;
 
 /**
- * Created by Admin on 15/04/2014.
+ * Class used to launch a local version of the player quiz system, with the file name passed by args[0]
+ *
+ * Created by Guilherme on 15/04/2014.
+ *
  */
 public class LaunchPlayerLocalVersion {
+    private static PlayerSaver ps;
+    private static PlayerLoader pl;
+
+    /**
+     * Launch the local player quiz system
+     *
+     * @param args pass in the source folder path
+     */
     public static void main(String[] args) {
-        PlayerSaver ps = new PlayerSaver(args[0]);
-        PlayerLoader pl = new PlayerLoader(args[0]);
         UserInterface ui = new UserInterface();
+        launch(args[0],ui);
+    }
+
+    /**
+     * alternative launching method for passing in a UserInterface.
+     * @param source the path of where the quiz system is located.
+     * @param ui param to pass in an instance of the UserInterface, opens the possibility for mocking.
+     */
+    public static void launch(String source, UserInterface ui) {
+        ps = new PlayerSaver(source);
+        pl = new PlayerLoader(source);
         PlayerLoginMenu plm = new PlayerLoginMenu(pl,ps,ui);
         plm.run();
     }
